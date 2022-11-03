b18c896a934718c6389c8733197e5f1ee55b73ee
Sangjin <sangjin170202@naver.com>
2022-11-03 20:13:48 +0900

    feat: UserRepository 추가

---
+++ src/main/java/com/indoor/woopy/domain/user/domain/repository/UserRepository.java
@@ -0,0 +1,13 @@
+package com.indoor.woopy.domain.user.domain.repository;
+
+import com.indoor.woopy.domain.user.domain.User;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends CrudRepository<User, Long> {
+
+    Optional<User> findByEmail(String email);
+}
