da07bc7ce96cf624dbf2bf5c020d11d30a3c89c2
Sangjin <sangjin170202@naver.com>
2022-11-03 16:13:49 +0900

    chore: 프로젝트 설정

---
+++ src/main/java/com/indoor/woopy/WoopyApplication.java
@@ -0,0 +1,13 @@
+package com.indoor.woopy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WoopyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WoopyApplication.class, args);
+	}
+
+}
