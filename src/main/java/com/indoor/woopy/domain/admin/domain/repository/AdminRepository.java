b92cb3a9641767f699f95f6297dc38a0a6d3d864
Sangjin <sangjin170202@naver.com>
2022-11-04 02:40:09 +0900

    fix: AdminRepository 추가 @Repository 어노테이션 추가

--- src/main/java/com/indoor/woopy/domain/admin/domain/repository/AdminRepository.java
+++ src/main/java/com/indoor/woopy/domain/admin/domain/repository/AdminRepository.java
@@ -2,9 +2,11 @@ package com.indoor.woopy.domain.admin.domain.repository;
 
 import com.indoor.woopy.domain.admin.domain.Admin;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
+@Repository
 public interface AdminRepository extends CrudRepository<Admin, Long> {
 
     Optional<Admin> findByEmail(String email);
