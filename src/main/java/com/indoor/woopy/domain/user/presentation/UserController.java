1381f1fd18d93f9a3d16456cf64c31cdd1fa671f
Sangjin <sangjin170202@naver.com>
2022-11-04 00:23:54 +0900

    feat: UserController 추가

---
+++ src/main/java/com/indoor/woopy/domain/user/presentation/UserController.java
@@ -0,0 +1,40 @@
+package com.indoor.woopy.domain.user.presentation;
+
+import com.indoor.woopy.domain.auth.presentation.dto.response.UserTokenResponse;
+import com.indoor.woopy.domain.user.presentation.dto.request.UserSignUpRequest;
+import com.indoor.woopy.domain.user.service.UserLogOutService;
+import com.indoor.woopy.domain.user.service.UserSignUpService;
+import com.indoor.woopy.domain.user.service.UserWithdrawalService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+
+@RequiredArgsConstructor
+@RequestMapping("/users")
+@RestController
+public class UserController {
+
+    private final UserSignUpService userSignUpService;
+    private final UserLogOutService userLogOutService;
+    private final UserWithdrawalService userWithdrawalService;
+
+    @ResponseStatus(HttpStatus.CREATED)
+    @PostMapping
+    public UserTokenResponse userSignUp(@RequestBody @Valid UserSignUpRequest request) {
+        return userSignUpService.execute(request);
+    }
+
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    @DeleteMapping("/logout")
+    public void logout() {
+        userLogOutService.execute();
+    }
+
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    @DeleteMapping("/leave")
+    public void leave() {
+        userWithdrawalService.execute();
+    }
+}
